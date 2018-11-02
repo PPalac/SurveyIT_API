16421a4e42c6fac64ae759fce24ef156f210e1ff
ppalac <ppalac@outlook.com>
2018-11-02 11:50:33 +0100

    added custom authorization attribute

---
+++ SurveyIT/SurveyIT/Attributes/Auth.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using SurveyIT.Enums;
+
+namespace SurveyIT.Attributes
+{
+    public class Auth : AuthorizeAttribute
+    {
+        public Auth(params Role[] roles)
+        {
+            Roles = string.Join(',', roles.Select(r => r.ToString()));
+        }
+    }
+}
