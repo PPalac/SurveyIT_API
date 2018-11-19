dbb63198c2ec6965a584be220e9473752e804578
Rafał <rafcio96@gmail.com>
2018-11-19 11:14:38 +0100

    Update DB
    Create three new Tables

---
+++ SurveyIT/SurveyIT/Models/DBModels/UserAnswers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SurveyIT.Models.DBModels
+{
+    public class UserAnswers
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+
+        public List<UserAnswers_List> UserAnswerList { get; set; }
+
+        public List<UsersLink> UserLinkL { get; set; }
+    }
+}
