a2de77c662a9b9a6004123fb7f5891442d4fb3f7
Rafał <rafcio96@gmail.com>
2018-11-19 11:15:09 +0100

    Added UserMaagement
    User Management by Admin

---
+++ SurveyIT/SurveyIT/Interfaces/Services/IUserManagementService.cs
@@ -0,0 +1,21 @@
+﻿using SurveyIT.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SurveyIT.Interfaces.Services
+{
+    public interface IUserManagementService
+    {
+        SortedList<string,string> DisplayAllUser();
+
+        SortedList<string, string> DisplayAllGroup();
+
+        SortedList<string, string> DisplayAssignedUsers(string groupId);
+
+        CommonResult AssignUsersToGroup(List<string> groupId, List<string> userId);
+
+        CommonResult UnAssignUsersInGroup(List<string> groupId, List<string> userId);
+    }
+}
